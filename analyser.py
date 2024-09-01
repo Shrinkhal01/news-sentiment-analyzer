4931dc678d98cadeca6e132265d87a1b381720f1
Shrinkhal <97280075+Shrinkhal01@users.noreply.github.com>
2024-09-01 13:04:28 +0530

    Update analyser.py

--- analyser.py
+++ analyser.py
@@ -2,7 +2,7 @@ from textblob import TextBlob
 from newspaper import Article
 import nltk
 nltk.download('punkt')
-url='https://www.cnbc.com/2024/08/31/warren-buffetts-berkshire-hathaway-sells-more-bank-of-america-has-now-cut-stake-by-nearly-15percent-.html'
+url='your-link-for-analysis'
 article=Article(url)
 article.download()
 article.parse()
